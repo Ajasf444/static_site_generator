d4ac62207f60de6a775eb0b88fc58f23f5c382ce
Ajasf444 <longstephen444@gmail.com>
2024-12-31 16:19:35 -0600

    created ParentNode class

---
+++ src/parentnode.py
@@ -0,0 +1,18 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props = None):
+        if tag is None:
+            raise ValueError("ParentNode must have a tag")
+        if children is None:
+            raise ValueError("ParentNode must have children")
+        if len(children) == 0:
+            raise ValueError("Parentnode must have at least one child")
+        super.__init__(tag, None, children, props)
+
+    def to_html(self):
+        children_html = ''
+        for child in self.children:
+            children_html += child.to_html()
+        return f'<self.tag>{children_html}</self.tag>'
+
