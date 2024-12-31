f2fdd1c7bca6905dd408f7995261380f476f5a36
Ajasf444 <longstephen444@gmail.com>
2024-12-31 08:22:06 -0600

    added initial state of project

---
+++ src/test_htmlnode.py
@@ -0,0 +1,21 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_get_tag(self):
+        node = HTMLNode(tag = 'baconator fries')
+        self.assertEqual(node.tag, 'baconator fries')
+    def test_get_value(self):
+        node = HTMLNode(value = 'baconator fries')
+        self.assertEqual(node.value, 'baconator fries')
+    def test_get_children(self):
+        node = HTMLNode(children = ['baconator', 'fries'])
+        self.assertEqual(node.children, ['baconator', 'fries'])
+    def test_props_to_html(self):
+        node = HTMLNode(props = {'baconator': 'fries', 'Tahm': 'Kench'})
+        self.assertEqual(node.props_to_html(), ' baconator="fries" Tahm="Kench"')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/test_leafnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_missing_value_should_error(self):
+        self.assertRaises(ValueError, lambda: LeafNode())
+    
+    def test_get_tag(self):
+        leaf_node = LeafNode('tag', 'value', {})
+        self.assertEqual(leaf_node.tag, 'tag')
+
+    def test_get_value(self):
+        leaf_node = LeafNode('tag', 'value', {})
+        self.assertEqual(leaf_node.value, 'value')
+
+    def test_props_to_html_without_tag(self):
+        leaf_node = LeafNode(value = 'Baconator Fries', props = {'Tahm': 'Kench'})
+        self.assertEqual(leaf_node.to_html(), 'Baconator Fries')
+    
+    def test_props_to_html_with_tag(self):
+        leaf_node = LeafNode('a', 'Baconator Fries', {'Tahm': 'Kench'})
+        correct_html = '<a Tahm="Kench">Baconator Fries</a>'
+        self.assertEqual(leaf_node.to_html(), correct_html)
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
