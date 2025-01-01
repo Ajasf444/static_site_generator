e562e781a55764e62c811c8e8d515267bad99fd8
Ajasf444 <longstephen444@gmail.com>
2025-01-01 16:07:32 -0600

    fixed f-string in to_html()

--- src/parentnode.py
+++ src/parentnode.py
@@ -8,11 +8,11 @@ class ParentNode(HTMLNode):
             raise ValueError("ParentNode must have children")
         if len(children) == 0:
             raise ValueError("Parentnode must have at least one child")
-        super.__init__(tag, None, children, props)
+        super().__init__(tag, None, children, props)
 
     def to_html(self):
         children_html = ''
         for child in self.children:
             children_html += child.to_html()
-        return f'<self.tag>{children_html}</self.tag>'
+        return f'<{self.tag}>{children_html}</{self.tag}>'
 
