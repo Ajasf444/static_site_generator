f3d32bf6f918f213caecd691e912fc6060fd8856
Ajasf444 <longstephen444@gmail.com>
2025-01-01 19:20:13 -0600

    removed extraneous bool() call

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,7 +9,7 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
-        if self.props is None or not bool(self.props):
+        if self.props is None or not self.props:
             return ''
         return ' ' + ' '.join(f'{key}="{value}"' for key, value in self.props.items())
     
