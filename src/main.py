9d2d65da1839dfa1d80ff16f906dbb8020341b66
Ajasf444 <longstephen444@gmail.com>
2025-01-01 22:17:47 -0600

    initial text_node_to_html_node definition

--- src/main.py
+++ src/main.py
@@ -5,8 +5,26 @@ def main():
     text_node = TextNode('This is a text node', TextType.BOLD, 'https://www.boot.dev')
     print(text_node)
 
-def text_node_to_leaf_node(text_node):
-    pass
+def text_node_to_html_node(text_node):
+    text_type = text_node.text_type
+    text = text_node.text
+    url = text_node.url
+
+    match text_type:
+        case NORMAL:
+            return LeafNode(None, text)
+        case BOLD:
+            return LeafNode('b', text)
+        case ITALIC:
+            return LeafNode('i', text)
+        case CODE:
+            return LeafNode('code', text)
+        case LINK:
+            return LeafNode('a', text)
+        case IMAGE:
+            return LeafNode('img', None, {'src': url, 'alt': text})
+        case _:
+            raise ValueError("Invalid TextType")
 
 if __name__ == '__main__':
     main()
