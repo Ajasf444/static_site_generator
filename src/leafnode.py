d359939ccc19254134469c5a4b17f0d9cf150156
Ajasf444 <longstephen444@gmail.com>
2025-01-02 14:10:07 -0600

    formatted file

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,12 +1,20 @@
 from htmlnode import HTMLNode
 
+
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value, props = None):
+    def __init__(self, tag, value, props=None):
         if value is None:
             raise ValueError
         super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.tag is None:
-            return f'{self.value}'
-        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+            return f"{self.value}"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __eq__(self, other):
+        return (
+            self.tag == other.tag
+            and self.value == other.value
+            and self.props == other.props
+        )
