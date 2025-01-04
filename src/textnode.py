ebec820a868c6a8d1ffce4df405bb282cbfea35a
Ajasf444 <longstephen444@gmail.com>
2025-01-04 17:38:37 -0600

    added split_nodes_image() and split_nodes_link()

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from leafnode import LeafNode
+from inline_operations import extract_markdown_images, extract_markdown_links
 
 
 class TextType(Enum):
@@ -54,11 +55,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     if not old_nodes:
         return new_nodes
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
             continue
-        split_text = node.text.split(delimiter)
+        split_text = old_node.text.split(delimiter)
         for i, text in enumerate(split_text):
             if not text:
                 continue
@@ -67,4 +68,52 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 new_node = TextNode(text, text_type)
             new_nodes.append(new_node)
+    return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    if not old_nodes:
+        return new_nodes
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        images = extract_markdown_images(old_node.text)
+        if not images:
+            new_nodes.append(old_node)
+            continue
+        remaining_text = old_node.text
+        for description, url in images:
+            split_text = remaining_text.split(f"![{description}]({url})", 1)
+            text, remaining_text = split_text[0], split_text[1]
+            if text:
+                new_nodes.append(TextNode(text, TextType.TEXT))
+            new_nodes.append(TextNode(description, TextType.IMAGE, url))
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    if not old_nodes:
+        return new_nodes
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        links = extract_markdown_links(old_node.text)
+        if not links:
+            new_nodes.append(old_node)
+            continue
+        remaining_text = old_node.text
+        for description, url in links:
+            split_text = remaining_text.split(f"[{description}]({url})", 1)
+            text, remaining_text = split_text[0], split_text[1]
+            if text:
+                new_nodes.append(TextNode(text, TextType.TEXT))
+            new_nodes.append(TextNode(description, TextType.LINK, url))
+        if remaining_text:
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
