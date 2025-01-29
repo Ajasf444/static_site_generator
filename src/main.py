4f69981fc1ac0273ed682fedd00f65b832fde2f6
Ajasf444 <longstephen444@gmail.com>
2025-01-29 08:42:00 -0600

    added first_pass flag to r_copy()

--- src/main.py
+++ src/main.py
@@ -3,7 +3,25 @@ import shutil
 
 
 def main():
-    pass
+    root = "blah"
+    dest = "Baconator_Fries"
+    r_copy(root, dest, True)
+
+
+def r_copy(root, dest, first_pass=False):
+    if first_pass:
+        if not os.path.exists(dest):
+            print(f"Creating directory: {dest}")
+            os.mkdir(dest)
+        else:
+            for item in os.listdir(dest):
+                path = os.path.join(dest, item)
+                if os.path.isfile(path):
+                    print(f"Removing file: {path}")
+                    os.remove(path)
+                else:
+                    print(f"Removing directory: {path}")
+                    shutil.rmtree(path)
 
 
 if __name__ == "__main__":
