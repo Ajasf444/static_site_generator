f566b4aaedbf6945738cecb11a31caa16a0d1b8a
Ajasf444 <longstephen444@gmail.com>
2025-01-29 11:53:48 -0600

    renamed path to src_path

--- src/main.py
+++ src/main.py
@@ -22,15 +22,15 @@ def r_copy(root, dest, first_pass=False):
                 else:
                     shutil.rmtree(path)
     for item in os.listdir(root):
-        path = os.path.join(root, item)
-        if os.path.isfile(path):
+        src_path = os.path.join(root, item)
+        if os.path.isfile(src_path):
             print(f"Copying {item} to {dest}")
-            shutil.copy(path, dest)
+            shutil.copy(src_path, dest)
         else:
             dest_path = os.path.join(dest, item)
             print(f"Making directory: {dest_path}")
             os.mkdir(dest_path)
-            r_copy(path, dest_path)
+            r_copy(src_path, dest_path)
 
 
 if __name__ == "__main__":
