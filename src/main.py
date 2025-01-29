7a104e898ca37ec757ad19fd28c99cf261d2813d
Ajasf444 <longstephen444@gmail.com>
2025-01-29 11:39:40 -0600

    modified root and dest to be run from main.sh

--- src/main.py
+++ src/main.py
@@ -3,16 +3,18 @@ import shutil
 
 
 def main():
-    root = "../static/"
-    dest = "../public/"
+    root = "static/"
+    dest = "public/"
     r_copy(root, dest, True)
 
 
 def r_copy(root, dest, first_pass=False):
     if first_pass:
         if not os.path.exists(dest):
+            print(f"Making directory: {dest}")
             os.mkdir(dest)
         else:
+            print(f"Removing contents of: {dest}")
             for item in os.listdir(dest):
                 path = os.path.join(dest, item)
                 if os.path.isfile(path):
