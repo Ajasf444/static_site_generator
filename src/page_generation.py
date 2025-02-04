8b37ce0ae39cc5a6ae276fe2f426f769fe3c36e7
Ajasf444 <longstephen444@gmail.com>
2025-02-04 03:50:44 -0600

    reading content and template

--- src/page_generation.py
+++ src/page_generation.py
@@ -8,3 +8,7 @@ def extract_title(markdown):
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as file:
+        content = file.read()
+    with open(template_path, "r") as file:
+        template = file.read()
