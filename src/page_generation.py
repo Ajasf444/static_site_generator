c98db9b08ef6ae1c2d2b555a9d843380740e4a33
Ajasf444 <longstephen444@gmail.com>
2025-02-05 04:18:39 -0600

    extracted title and renamed "content" to "markdown"

--- src/page_generation.py
+++ src/page_generation.py
@@ -12,7 +12,8 @@ def extract_title(markdown):
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as file:
-        content = file.read()
+        markdown = file.read()
     with open(template_path, "r") as file:
         template = file.read()
-    html = markdown_to_html_node(content).to_html()
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
