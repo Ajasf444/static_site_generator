9aa9fc6ef85d9385d79696bc8711c16449b368c1
Ajasf444 <longstephen444@gmail.com>
2025-02-08 03:37:59 -0600

    refactored recursive deletion step in recursive_copy()

--- src/main.py
+++ src/main.py
@@ -5,23 +5,12 @@ import shutil
 def main():
     root = "static/"
     dest = "public/"
-    # TODO: delete contents of public/
-    r_copy(root, dest, True)
+    recursive_copy(root, dest, True)
 
 
-def r_copy(root, dest, first_pass=False):
+def recursive_copy(root, dest, first_pass=False):
     if first_pass:
-        if not os.path.exists(dest):
-            print(f"Making directory: {dest}")
-            os.mkdir(dest)
-        else:
-            print(f"Removing contents of: {dest}")
-            for item in os.listdir(dest):
-                path = os.path.join(dest, item)
-                if os.path.isfile(path):
-                    os.remove(path)
-                else:
-                    shutil.rmtree(path)
+        recursive_rm(dest)
     for item in os.listdir(root):
         src_path = os.path.join(root, item)
         if os.path.isfile(src_path):
@@ -31,7 +20,21 @@ def r_copy(root, dest, first_pass=False):
             dest_path = os.path.join(dest, item)
             print(f"Making directory: {dest_path}")
             os.mkdir(dest_path)
-            r_copy(src_path, dest_path)
+            recursive_copy(src_path, dest_path)
+
+
+def recursive_rm(dest):
+    if not os.path.exists(dest):
+        print(f"Making directory: {dest}")
+        os.mkdir(dest)
+    else:
+        print(f"Removing contents of: {dest}")
+        for item in os.listdir(dest):
+            path = os.path.join(dest, item)
+            if os.path.isfile(path):
+                os.remove(path)
+            else:
+                shutil.rmtree(path)
 
 
 if __name__ == "__main__":
